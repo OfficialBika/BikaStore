d79242e1d1d7a03654220c5acb1ab5224a46c655
Official Bika <phoetaay@gmail.com>
2026-01-24 20:22:49 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,1 @@
-const fetch = require("node-fetch");
 
-const token = process.env.BOT_TOKEN;
-const chatId = process.env.CHAT_ID;
-
-async function sendOrder(message) {
-  await fetch(`https://api.telegram.org/bot${token}/sendMessage`, {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({
-      chat_id: chatId,
-      text: message
-    })
-  });
-}
