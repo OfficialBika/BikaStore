da239b6d35b014eeece5db98174c07b74733c741
Official Bika <phoetaay@gmail.com>
2026-01-27 18:30:38 +0700

    Update Order.js

--- src/models/Order.js
+++ src/models/Order.js
@@ -1,1 +1,61 @@
+const mongoose = require("mongoose");
 
+const OrderSchema = new mongoose.Schema(
+  {
+    orderId: {
+      type: String,
+      required: true,
+      unique: true
+    },
+
+    userId: {
+      type: String,
+      required: true
+    },
+
+    userRef: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User"
+    },
+
+    username: String,
+
+    product: {
+      type: String,
+      required: true
+    },
+
+    gameId: String,
+    serverId: String,
+
+    items: {
+      type: Array,
+      default: []
+    },
+
+    totalPrice: {
+      type: Number,
+      required: true
+    },
+
+    paymentMethod: String,
+    paymentPhoto: String,
+
+    status: {
+      type: String,
+      enum: ["PENDING", "COMPLETED", "REJECTED"],
+      default: "PENDING"
+    },
+
+    waitMsgId: Number,
+    adminMsgId: Number,
+    adminChatId: String,
+
+    approvedAt: Date,
+
+    expireAt: Date
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Order", OrderSchema);
