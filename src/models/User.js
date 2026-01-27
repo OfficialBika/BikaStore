e3a8d03bdd2c328a23aab074ed245208b8ed29c6
Official Bika <phoetaay@gmail.com>
2026-01-27 18:29:24 +0700

    Update User.js

--- src/models/User.js
+++ src/models/User.js
@@ -1,1 +1,22 @@
+const mongoose = require("mongoose");
 
+const UserSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+      unique: true
+    },
+    username: {
+      type: String
+    },
+    role: {
+      type: String,
+      enum: ["USER", "ADMIN"],
+      default: "USER"
+    }
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("User", UserSchema);
