eb0df5335b00711b6d93f2cf9f8b5e93a980e4a3
Official Bika <phoetaay@gmail.com>
2026-01-29 18:13:29 +0700

    Create promo.js

---
+++ src/models/promo.js
@@ -0,0 +1,37 @@
+// ===============================
+// PROMOTION MODEL (SINGLE CLAIM)
+// ===============================
+
+const promo = {
+  active: false,     // promo is running
+  claimed: false,    // already claimed?
+
+  winner: {
+    chatId: null,
+    username: null
+  },
+
+  gameId: null,
+  serverId: null,
+
+  approved: false
+};
+
+// ===============================
+function resetPromo() {
+  promo.active = false;
+  promo.claimed = false;
+
+  promo.winner.chatId = null;
+  promo.winner.username = null;
+
+  promo.gameId = null;
+  promo.serverId = null;
+
+  promo.approved = false;
+}
+
+module.exports = {
+  promo,
+  resetPromo
+};
