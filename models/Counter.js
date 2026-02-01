6ddca2e6958ee7aa33e94ca4176cbacd75cc2c25
Official Bika <phoetaay@gmail.com>
2026-02-01 11:49:15 +0700

    Update Counter.js

--- models/Counter.js
+++ models/Counter.js
@@ -1,7 +1,12 @@
-// models/Counter.js — Counter Tracking Schema
+// models/Counter.js — For Order Number Auto-Increment
 
 const mongoose = require("mongoose");
 
-const counterSchema = new mongoose.Schema( { key: { type: String, required: true, unique: true }, value: { type: Number, default: 0 }, }, { timestamps: true } );
+const counterSchema = new mongoose.Schema({
+  name: { type: String, required: true, unique: true },
+  value: { type: Number, default: 1000 },
+});
 
-module.exports = mongoose.model("Counter", counterSchema);
+module.exports = {
+  Counter: mongoose.model("Counter", counterSchema),
+};
